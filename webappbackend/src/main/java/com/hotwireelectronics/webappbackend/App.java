34ec8a7d6013c8a3c7cbdd2322f7bb312a38c168
Barry Jackson <jayfive30@gmail.com>
2018-02-09 12:42:55 -0600

    My Project setup completed successfully.

---
+++ webappbackend/src/main/java/com/hotwireelectronics/webappbackend/App.java
@@ -0,0 +1,13 @@
+package com.hotwireelectronics.webappbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
